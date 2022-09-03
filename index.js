d7b3ca805499ec3f3c2a204f9b1c62c31f94cd65
vinayak500 <vinayakshetty500@gmail.com>
2022-09-03 23:37:14 +0530

    added home  page  view

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const port = 8000;
 app.use('/' , require('./routes/index'));
 
 
+//tell the app we are using ejs as view engine , and search for ejs file in views folder
+app.set('view engine' , 'ejs');
+app.set('views' , './views');
 
 
 app.listen(port , function(err){
